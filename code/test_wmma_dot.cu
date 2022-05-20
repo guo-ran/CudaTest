89c175e4e3abbfcd226a65525267a3a727ff090e
guo-ran <360112263@qq.com>
2022-05-20 20:12:53 +0800

    right result

--- code/test_wmma_dot.cu
+++ code/test_wmma_dot.cu
@@ -25,16 +25,18 @@ struct alignas(sizeof(T) * pack_size) Pack {
   T elem[pack_size];
 };
 
-
 constexpr int num_warp_per_block = 4;
 constexpr int padded_num_rows = 32;
 constexpr int skew_half = 8; // for align and bank conflict
 constexpr int shared_mem_num_cols = 128 + skew_half;
 constexpr int shared_mem_num_cols_pack4 = shared_mem_num_cols / 4;
 constexpr int shared_mem_num_cols_acc = 32 + skew_half;
-constexpr int in_shared_mem_bytes = padded_num_rows * shared_mem_num_cols_acc * sizeof(half);
-constexpr int acc_shared_mem_stride_bytes = padded_num_rows * shared_mem_num_cols_acc * sizeof(float);
-constexpr int acc_shared_mem_bytes = acc_shared_mem_stride_bytes * num_warp_per_block;
+constexpr int in_shared_mem_bytes =
+    padded_num_rows * shared_mem_num_cols_acc * sizeof(half);
+constexpr int acc_shared_mem_stride_bytes =
+    padded_num_rows * shared_mem_num_cols_acc * sizeof(float);
+constexpr int acc_shared_mem_bytes =
+    acc_shared_mem_stride_bytes * num_warp_per_block;
 constexpr int TILE_DIM = 16;
 constexpr int M_BLOCKS = 2;
 constexpr int K_BLOCKS = 8;
@@ -50,20 +52,17 @@ __global__ void DotFeatureInteraction(int batch_size, int embedding_size,
                                       Param<half, N> param) {
   extern __shared__ __align__(sizeof(double)) unsigned char shared_buf[];
   int warp_id = threadIdx.y;
-  half *buf =
-      reinterpret_cast<half *>(shared_buf);
+  half *buf = reinterpret_cast<half *>(shared_buf);
   Pack<half, 4> *buf_pack4 = reinterpret_cast<Pack<half, 4> *>(shared_buf);
-  float *acc_buf =
-      reinterpret_cast<float *>(shared_buf);
+  float *acc_buf = reinterpret_cast<float *>(shared_buf);
   for (int batch_idx = blockIdx.x; batch_idx < batch_size;
        batch_idx += gridDim.x) {
     half *batch_out = param.out + batch_idx * out_num_cols;
     Pack<half, 4> *batch_out_pack4 =
         reinterpret_cast<Pack<half, 4> *>(param.out) +
         batch_idx * out_num_cols_pack4;
-    __syncthreads();
     // 1. load in to shared
-    if(warp_id == 0) {
+    if (warp_id == 0) {
       for (int i = 0; i < N; ++i) {
         if (i >= param.num_in) {
           break;
@@ -72,7 +71,7 @@ __global__ void DotFeatureInteraction(int batch_size, int embedding_size,
         const Pack<half, 4> *batch_in =
             reinterpret_cast<const Pack<half, 4> *>(param.in[i]) +
             batch_idx * param.in_feature_dim[i] * embedding_num_pack;
-  #pragma unroll
+#pragma unroll
         for (int j = 0; j < param.in_feature_dim[i]; ++j) {
           int row = start_offset + j;
           // for (int col = threadIdx.x; col < embedding_num_pack; col
@@ -88,7 +87,8 @@ __global__ void DotFeatureInteraction(int batch_size, int embedding_size,
         zero.elem[k] = 0;
       }
       for (int i = 27; i < padded_num_rows; ++i) {
-        for (int col = threadIdx.x; col < embedding_num_pack; col += blockDim.x) {
+        for (int col = threadIdx.x; col < embedding_num_pack;
+             col += blockDim.x) {
           buf_pack4[i * shared_mem_num_cols_pack4 + col] = zero;
         }
       }
@@ -97,15 +97,7 @@ __global__ void DotFeatureInteraction(int batch_size, int embedding_size,
       }
     }
     __syncthreads();
-    //if(warp_id==0 && threadIdx.x==0&&blockIdx.x==0) {
-    //  for(int i=0;i<padded_num_rows;++i) {
-    //    printf("\n");
-    //    for(int j=0;j<32;++j) {
-    //      printf("%f ", static_cast<float>(buf[i*shared_mem_num_cols+j]));
-    //    }
-    //  }
-    //}
-    __syncthreads();
+
     // 2. load to tensor core
     nvcuda::wmma::fragment<nvcuda::wmma::accumulator, TILE_DIM, TILE_DIM,
                            TILE_DIM, float>
@@ -115,17 +107,17 @@ __global__ void DotFeatureInteraction(int batch_size, int embedding_size,
         nvcuda::wmma::fill_fragment(acc[i][j], 0.0f);
       }
     }
-    nvcuda::wmma::fragment<nvcuda::wmma::matrix_a, TILE_DIM, TILE_DIM, TILE_DIM,
-                           half, nvcuda::wmma::row_major>
-        a[M_BLOCKS];
-    nvcuda::wmma::fragment<nvcuda::wmma::matrix_b, TILE_DIM, TILE_DIM, TILE_DIM,
-                           half, nvcuda::wmma::col_major>
-        b[M_BLOCKS];
-        
+
     for (int step = 0; step < 2; ++step) {
+      nvcuda::wmma::fragment<nvcuda::wmma::matrix_a, TILE_DIM, TILE_DIM,
+                             TILE_DIM, half, nvcuda::wmma::row_major>
+          a[M_BLOCKS];
+      nvcuda::wmma::fragment<nvcuda::wmma::matrix_b, TILE_DIM, TILE_DIM,
+                             TILE_DIM, half, nvcuda::wmma::col_major>
+          b[M_BLOCKS];
       for (int j = 0; j < M_BLOCKS; ++j) {
-        half *tile_ptr =
-            buf + j * TILE_DIM * shared_mem_num_cols + (2 * warp_id + step) * TILE_DIM;
+        half *tile_ptr = buf + j * TILE_DIM * shared_mem_num_cols +
+                         (2 * warp_id + step) * TILE_DIM;
         nvcuda::wmma::load_matrix_sync(a[j], tile_ptr, shared_mem_num_cols);
         nvcuda::wmma::load_matrix_sync(b[j], tile_ptr, shared_mem_num_cols);
       }
@@ -138,38 +130,32 @@ __global__ void DotFeatureInteraction(int batch_size, int embedding_size,
         }
       }
     }
+    __syncthreads();
+
     int elem_cnt_per_warp = padded_num_rows * shared_mem_num_cols_acc;
     for (int i = 0; i < M_BLOCKS; i++) {
       for (int j = 0; j < M_BLOCKS; j++) {
         // if (i < j) {
         //  continue;
         //}
-        float *tile_ptr =
-            acc_buf + warp_id * elem_cnt_per_warp + i * TILE_DIM * shared_mem_num_cols_acc + j * TILE_DIM;
+        float *tile_ptr = acc_buf + warp_id * elem_cnt_per_warp +
+                          i * TILE_DIM * shared_mem_num_cols_acc + j * TILE_DIM;
         nvcuda::wmma::store_matrix_sync(tile_ptr, acc[i][j],
                                         shared_mem_num_cols_acc,
                                         nvcuda::wmma::mem_row_major);
       }
     }
     __syncthreads();
-    //if(warp_id==0 && threadIdx.x==0&&blockIdx.x==1) {
-    //  for(int i=0;i<padded_num_rows*4;++i) {
-    //    printf("\n");
-    //    for(int j=0;j<1;++j) {
-    //      printf("%f ", static_cast<float>(acc_buf[i*shared_mem_num_cols_acc+j]));
-    //    }
-    //  }
-    //}
-    //__syncthreads();
-    //acc 4 32x32
     int thread_id = threadIdx.y * blockDim.x + threadIdx.x;
-    for(int k=1;k<num_warp_per_block;++k) {
-      for(int i=thread_id;i<elem_cnt_per_warp;i+=blockDim.x*blockDim.y) {
-        acc_buf[i] += acc_buf[k*elem_cnt_per_warp+i];
+    for (int k = 1; k < num_warp_per_block; ++k) {
+      int warp_offset = k * elem_cnt_per_warp;
+      for (int i = thread_id; i < elem_cnt_per_warp;
+           i += blockDim.x * blockDim.y) {
+        acc_buf[i] += acc_buf[warp_offset + i];
       }
     }
     __syncthreads();
-    if(warp_id == 0) {
+    if (warp_id == 0) {
       half *emb_out = batch_out + embedding_size;
       for (int row = 0; row < M_BLOCKS * TILE_DIM; ++row) {
         for (int col = threadIdx.x; col < M_BLOCKS * TILE_DIM;
@@ -199,7 +185,7 @@ __global__ void DotFeatureInteraction(int batch_size, int embedding_size,
 
 int main() {
   using T = half; // int
-  int64_t batch_size = 128;//55296 / 8;
+  int64_t batch_size = 55296 / 8;
   int64_t vector_size = 128;
   int64_t vector_num_pack = vector_size / 4;
   std::vector<int64_t> feature_dims = {1, 26};
@@ -259,10 +245,14 @@ int main() {
 
   for (int i = 0; i < batch_size * out_dim; i++) {
     int batch_idx = i / out_dim;
+    int out_i = i % out_dim;
     float diff = static_cast<float>(host_out_ptr[i]) -
                  static_cast<float>(out_data.at(i));
-    if (diff > 0.01) {
-      std::cout << "i " << i << " batch_idx" <<batch_idx<<" diff " << diff <<" out0: "<<static_cast<float>(host_out_ptr[i]) <<" out1 "<<static_cast<float>(out_data.at(i))<< std::endl;
+    if (diff > 0.1) {
+      std::cout << "i " << i << " batch_idx" << batch_idx << " out_i " << out_i
+                << " diff " << diff
+                << " out0: " << static_cast<float>(host_out_ptr[i]) << " out1 "
+                << static_cast<float>(out_data.at(i)) << std::endl;
     }
   }
 
