3b6aa616ad5dc56863fb405fb64449a05036d55f
guo-ran <360112263@qq.com>
2022-07-25 18:52:13 +0800

    add float impl

--- code/interaction_sum/test_row_reduce.cu
+++ code/interaction_sum/test_row_reduce.cu
@@ -135,6 +135,27 @@ __global__ void fm_order2_kernel(const __half *in, __half *out, int batch_size,
   }
 }
 
+__global__ void fm_order2_dgrad_kernel(const float* in, const float* top_grad, float* dgrad,
+                                       int batch_size, int slot_num, int emb_vec_size) {
+  int tid = threadIdx.x;
+  int bid = blockIdx.x;
+
+  if (tid < emb_vec_size && bid < batch_size) {
+    float emb_sum = 0.0f;
+    int offset = bid * slot_num * emb_vec_size + tid;
+
+    for (int i = 0; i < slot_num; i++) {
+      int index = offset + i * emb_vec_size;
+      emb_sum += in[index];
+    }
+    float tgrad = top_grad[bid * emb_vec_size + tid];
+    for (int i = 0; i < slot_num; i++) {
+      int index = offset + i * emb_vec_size;
+      dgrad[index] = tgrad * (emb_sum - in[index]);
+    }
+  }
+}
+
 // hugectr impl
 __global__ void fm_order2_dgrad_kernel(const __half *in, const __half *top_grad,
                                        __half *dgrad, int batch_size,
